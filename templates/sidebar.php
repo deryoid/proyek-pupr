9d45643251deabb6d23ad5fce8d1d5e773a309c5
derylatom <dery037yj@gmail.com>
2021-07-11 10:24:35 +0800

    okeyy

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -126,14 +126,14 @@
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a href="<?= base_url('admin/detailproyek/') ?>" class="nav-link">
               <i class="nav-icon fas fa-truck-monster"></i>
               <p>
                 Detail Barang Proyek
               </p>
             </a>
-          </li>
+          </li> -->
 
         </ul>
       </nav>
